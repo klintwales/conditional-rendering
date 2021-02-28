fea0a240c4b45199fce4883cee60c970f15a54c3
Klint <klintwales@gmail.com>
2021-02-28 16:09:12 -0600

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./login";
+
+var isLoggedIn = false;
+
+function App() {
+  return (
+    <div className="container">
+    {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
+    }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/input.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function input(props){
+  return(
+    <div>
+    <input type={props.type} placeholder={props.placeholder}/>
+    </div>
+  )
+}
+
+
+
+
+export default input;
